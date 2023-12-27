068a12de666555715360f54d8df722afdebc2e7a
Leandro Amaral <lramaral02m@gmail.com>
2023-12-27 15:28:10 -0300

    Criando as classes Role e Usuarios, e configurado conexão com o banco de dados

--- src/main/java/com/gerenciadortarefas/controller/TesteApiController.java
+++ src/main/java/com/gerenciadortarefas/controller/TesteApiController.java
@@ -1,6 +1,7 @@
 package com.gerenciadortarefas.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +9,9 @@ public class TesteApiController {
     @GetMapping("/teste-api")
     private String teste() {
         return "Sucesso!";
+    }
+    @GetMapping("/saudacao")
+    public String Saudacao(@RequestParam(name = "nome") String nome) {
+        return "Olá! " + nome + ", Seja bem vindo!";
     }
 }
