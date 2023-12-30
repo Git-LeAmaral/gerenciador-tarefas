37b8668b078180dbae8b7e3db8a95c53a6cd2d37
Leandro Amaral <lramaral02m@gmail.com>
2023-12-30 17:12:27 -0300

    concluindo classe Tarefas

--- src/main/java/com/gerenciadortarefas/entity/Tarefa.java
+++ src/main/java/com/gerenciadortarefas/entity/Tarefa.java
@@ -1,11 +1,12 @@
 package com.gerenciadortarefas.entity;
 
 import com.gerenciadortarefas.status.TarefaStatusEnum;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.time.LocalTime;
@@ -19,16 +20,41 @@ public class Tarefa implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
+
+    @Column(nullable = false)
     private String titulo;
+
+    @Column(nullable = false)
     private String descricao;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private TarefaStatusEnum status;
+
+    @Column(nullable = false)
     private Usuario responsavel;
+
+    @Column(nullable = false)
     private Usuario criador;
+
+    @Column(nullable = false)
     private int quantidadeHosrasEstimadas;
+
+    @Column
     private Integer quantidadeHorasRealizadas;
+
+    @Column
+    @CreationTimestamp
     private LocalTime dataCadastro;
+
+    @Column
+    @UpdateTimestamp
     private LocalTime dataAtualizacao;
+
+    @Column
     private LocalTime tempoRealizado;
 
 
